2eb86df4d6841257323d083613f4de6ee3825320
Sourabh kadam <saurabhkadam612a@gmail.com>
2023-03-26 00:07:46 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -9,17 +9,17 @@ from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-def save_object(file_path, obj):
+def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path, exist_ok=True)
+        os.makedirs(dir_path,exist_ok=True)
 
-        with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
     
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
@@ -54,8 +54,8 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     
 def load_object(file_path):
     try:
-        with open(file_path, "rb") as file_obj:
+        with open(file_path,"rb") as file_obj:
             return dill.load(file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        raise CustomException(e,sys)
